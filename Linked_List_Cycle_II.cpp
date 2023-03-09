5d3c278ac2482aa4747b7e9bcba921cf29b8443e
VaishnaviInamdar <58802277+VaishnaviInamdar@users.noreply.github.com>
2023-03-09 23:18:39 +0530

    Create Linked_List_Cycle_II.cpp

---
+++ Linked_List_Cycle_II.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        if(!head || !(head->next))
+            return NULL;
+        
+        ListNode* slow = head;
+        ListNode* fast = head;
+        
+        while(fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow == fast)
+                break;
+        }
+        
+        if(fast != slow)
+            return NULL;
+        
+        ListNode* entry = head;
+        while(entry != slow) {
+            entry = entry->next;
+            slow  = slow->next;
+        }
+        return entry;
+    }
+};
